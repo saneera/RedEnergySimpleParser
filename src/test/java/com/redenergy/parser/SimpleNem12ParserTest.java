15b2f43aa68f72b168a6e5e0f0d4cc58e9c0bc87
syapa <saneera@gmail.com>
2018-08-14 11:49:20 +0930

    first commit

--- src/test/java/com/redenergy/parser/SimpleNem12ParserTest.java
+++ src/test/java/com/redenergy/parser/SimpleNem12ParserTest.java
@@ -1,10 +1,16 @@
 package com.redenergy.parser;
 
 import java.io.File;
+import java.math.BigDecimal;
+import java.util.Collection;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 import com.redenergy.csv.CsvReader;
+import com.redenergy.model.EnergyUnit;
+import com.redenergy.model.MeterRead;
 import com.redenergy.parser.validate.SimpleNemParserValidator;
 import com.redenergy.parser.validate.SimpleNemParserValidatorImpl;
 
@@ -18,6 +24,7 @@ public class SimpleNem12ParserTest {
 	private CsvReader csvReader;
 	private SimpleNem12ParserImpl nem12ParserImpl;
 	private SimpleNemParserValidator validator;
+	private File validCsvFile;
 	
 	@Before
 	public void setUp() throws Exception {
@@ -25,14 +32,32 @@ public class SimpleNem12ParserTest {
 		nem12ParserImpl = new SimpleNem12ParserImpl();
 		validator = new SimpleNemParserValidatorImpl();
 		ClassLoader classLoader = getClass().getClassLoader();		
-		File file  = new File(classLoader.getResource("SimpleNem12.csv").getFile());
-		csvReader.setCsvFile(file);
+		validCsvFile  = new File(classLoader.getResource("SimpleNem12.csv").getFile());
+		csvReader.setCsvFile(validCsvFile);
 		nem12ParserImpl.setCsvReader(csvReader);
 		nem12ParserImpl.setValidator(validator);		
 	}
 	
 	
-	
+	  @Test
+	  public void testMeterReadsWithValidCSV() throws Exception {
+		  Collection<MeterRead> meterReads = nem12ParserImpl.parseSimpleNem12(validCsvFile);
+		  Assert.assertNotNull(meterReads);
+		  MeterRead firstRecord = meterReads.stream().findFirst().get();
+		  Assert.assertNotNull(firstRecord);
+		  Assert.assertEquals("6123456789", firstRecord.getNmi());
+		  Assert.assertEquals(EnergyUnit.KWH, firstRecord.getEnergyUnit());
+		  Assert.assertNotNull(firstRecord.getVolumes());
+		  Assert.assertEquals(7, firstRecord.getVolumes().size());
+		  
+		  MeterRead read6123456789 = meterReads.stream().filter(mr -> mr.getNmi().equals("6123456789")).findFirst().get();
+		  Assert.assertEquals(new BigDecimal("-36.84"), read6123456789.getTotalVolume());
+			 
+	      MeterRead read6987654321 = meterReads.stream().filter(mr -> mr.getNmi().equals("6987654321")).findFirst().get();
+	      Assert.assertEquals(new BigDecimal("14.33"), read6987654321.getTotalVolume());
+		  
+		  
+	  }
 	
 	
 	
